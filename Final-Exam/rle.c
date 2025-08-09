d5ef2f656a4da72f5087af1f24d1a6bcf0a48847
ossamatouayri <ossamatwiri2003@gmail.com>
2025-08-09 18:45:39 +0100

    boxee99

--- Final-Exam/rle.c
+++ Final-Exam/rle.c
@@ -1,6 +1,29 @@
 #include <unistd.h>
 #include <stdio.h>
 
+void ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void ft_putnbr(int n)
+{
+	if(n < 0)
+	{
+		n *= -1;
+		ft_putnbr(n);
+	}
+	else if (n < 10)
+	{
+		ft_putchar(n + '0');
+	}
+	else
+	{
+		ft_putnbr(n / 10);
+		ft_putnbr(n % 10);
+	}
+}
+
 int check_func(char **av)
 {
 	int i = 0;
@@ -18,23 +41,26 @@ int main(int ac, char **av)
 	if (ac == 2)
 	{
 		if (!check_func(av))
-			return (printf("\n"),0);
-		int i = 0;
-		int count = 0;
+			return (ft_putchar('\n'),0);
+		int i;
+		int count;
+		char c;
+
+		i = 0;
+		c = '\0';
 		while (av[1][i])
 		{
 			count = 0;
-			char c = av[1][i];
+			c = av[1][i];
 			while (av[1][i] == c)
 			{
-				count++;
 				i++;
+				count++;
 			}
 			if (count > 1)
-				printf("%d%c", count, c);
-			else
-				printf("%c", c);
+				ft_putnbr(count);
+			ft_putchar(c);
 		}
 	}
-	printf("\n");
+	ft_putchar('\n');
 }
