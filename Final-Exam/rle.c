be928e4214f7dac82fcd11aa6941b48dd8014c48
ossamatouayri <ossamatwiri2003@gmail.com>
2025-09-05 18:30:06 +0100

    boxee99

--- Final-Exam/rle.c
+++ Final-Exam/rle.c
@@ -6,48 +6,15 @@ void ft_putchar(char c)
 	write(1, &c, 1);
 }
 
-void ft_putnbr(int n)
-{
-	if(n < 0)
-	{
-		n *= -1;
-		ft_putnbr(n);
-	}
-	else if (n < 10)
-	{
-		ft_putchar(n + '0');
-	}
-	else
-	{
-		ft_putnbr(n / 10);
-		ft_putnbr(n % 10);
-	}
-}
-
-int check_func(char **av)
-{
-	int i = 0;
-	while (av[1][i])
-	{
-		if (av[1][i] < 'A' || av[1][i] > 'Z')
-			return (0);
-		i++;
-	}
-	return (1);
-}
-
 int main(int ac, char **av)
 {
 	if (ac == 2)
 	{
-		if (!check_func(av))
-			return (ft_putchar('\n'),0);
 		int i;
 		int count;
 		char c;
 
 		i = 0;
-		c = '\0';
 		while (av[1][i])
 		{
 			count = 0;
@@ -58,7 +25,7 @@ int main(int ac, char **av)
 				count++;
 			}
 			if (count > 1)
-				ft_putnbr(count);
+				ft_putchar(count + '0');
 			ft_putchar(c);
 		}
 	}
